3838cffdd6b4dec9692a714a2018c9690faad621
venkateshsure <venky007sure@gmail.com>
2026-04-10 15:20:28 +0530

    changes for the viteconfig.ts file

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
