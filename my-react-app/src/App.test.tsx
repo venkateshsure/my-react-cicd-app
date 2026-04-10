be9adc3861416c86d25837a44874b7f9d2d3289b
venkateshsure <venky007sure@gmail.com>
2026-04-10 14:49:04 +0530

    change the fiel name from setUpTests.ts to setupTests.ts

--- my-react-app/src/App.test.tsx
+++ my-react-app/src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+    import { render, screen } from "@testing-library/react";
+    import App from "./App";
 
-test("renders heading text", () => {
-  render(<App />);
-  const text = screen.getByText(/get started/i);
-  expect(text).toBeInTheDocument();
-});
+    test("renders heading text", () => {
+    render(<App />);
+    const text = screen.getByText(/get started/i);
+    expect(text).toBeInTheDocument();
+    });
