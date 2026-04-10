8c06b410769f09252c195c59c6e569eb6dd91ee9
venkateshsure <venky007sure@gmail.com>
2026-04-10 14:54:52 +0530

    updated the setupFile path

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.ts', // ✅ IMPORTANT
+    setupFiles: 'src/setupTests.ts', // ✅ IMPORTANT
   },
 })
