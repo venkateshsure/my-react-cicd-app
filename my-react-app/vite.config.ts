396ab4002e875a3ac1d329fb41d8f9c1e3b54e4c
venkateshsure <venky007sure@gmail.com>
2026-04-16 16:20:15 +0530

    change the base field path in vite.ts

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', //'/my-react-cicd-app/', // repo name
+  base: '/my-react-cicd-app/', // repo name
   plugins: [react()],
   test: {
     environment: 'jsdom',
